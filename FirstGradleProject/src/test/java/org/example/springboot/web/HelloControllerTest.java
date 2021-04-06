126b793cb3a773ee8ff83a089da6614655f9815f
sechiyo97 <sechiyo97@yonsei.ac.kr>
2021-04-07 04:55:20 +0900

    DB 생성 및 insert, update, select 추가

--- FirstGradleProject/src/test/java/org/example/springboot/web/HelloControllerTest.java
+++ FirstGradleProject/src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -8,8 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)

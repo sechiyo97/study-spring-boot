91ab32c983f1c3b6c9bbc9fb7a01e4d5d2a9dc10
sechiyo97 <sechiyo97@yonsei.ac.kr>
2021-05-12 02:16:59 +0900

    테스트 메소드명 변경 및 테스트의 시큐리티 문제 수정

--- FirstGradleProject/src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
+++ FirstGradleProject/src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -25,7 +25,7 @@ public class PostsRepositoryTest {
     }
 
     @Test
-    public void saveAndLoadPost() {
+    public void 게시글저장_불러오기() {
         // given
         String title = "테스트 게시글";
         String content = "테스트 본문";
@@ -46,7 +46,7 @@ public class PostsRepositoryTest {
     }
 
     @Test
-    public void BaseTimeEntity_insert() {
+    public void BaseTimeEntity_등록() {
         // given
         LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
         postsRepository.save(Posts.builder()
--- FirstGradleProject/src/test/java/org/example/springboot/web/HelloControllerTest.java
+++ FirstGradleProject/src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package org.example.springboot.web;
 
+import org.example.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,13 +19,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+    }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
@@ -31,6 +40,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
